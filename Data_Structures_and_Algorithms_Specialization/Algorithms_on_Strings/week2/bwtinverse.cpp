e5dcf0d5d5b7b9d85a97bd1c6568f45a1924b8d2
Younghoon Jung <69005067+younghoonJ@users.noreply.github.com>
2023-04-17 20:54:31 +0900

    Update bwtinverse.cpp

--- Data_Structures_and_Algorithms_Specialization/Algorithms_on_Strings/week2/bwtinverse.cpp
+++ Data_Structures_and_Algorithms_Specialization/Algorithms_on_Strings/week2/bwtinverse.cpp
@@ -12,26 +12,49 @@ using std::vector;
 string InverseBWT(const string &bwt) {
     if (bwt.size() == 1) return bwt;
 
-    size_t pos_a = 0, pos_c = 0, pos_g = 0, pos_t;
-    for (auto &c: bwt) {
-        if (c == 'A') ++pos_a;
-        else if (c == 'C') ++pos_c;
-        else if (c == 'G') ++pos_g;
+    std::vector<int> a_before(bwt.size() + 1, 0);
+    std::vector<int> c_before(bwt.size() + 1, 0);
+    std::vector<int> g_before(bwt.size() + 1, 0);
+    std::vector<int> t_before(bwt.size() + 1, 0);
+
+    int cnt_a = 0, cnt_c = 0, cnt_g = 0, cnt_t;
+    for (size_t i = 0; i < bwt.size(); ++i) {
+        a_before[i + 1] = a_before[i];
+        c_before[i + 1] = c_before[i];
+        g_before[i + 1] = g_before[i];
+        t_before[i + 1] = t_before[i];
+
+        auto c = bwt[i];
+        if (c == 'A') {
+            ++cnt_a;
+            ++a_before[i + 1];
+        } else if (c == 'C') {
+            ++cnt_c;
+            ++c_before[i + 1];
+        } else if (c == 'G') {
+            ++cnt_g;
+            ++g_before[i + 1];
+        } else if (c == 'T') {
+            ++t_before[i + 1];
+        }
+
     }
-    pos_t = 1 + pos_a + pos_c + pos_g;
-    pos_g = 1 + pos_a + pos_c;
-    pos_c = 1 + pos_a;
-    pos_a = 1;
+    cnt_t = 1 + cnt_a + cnt_c + cnt_g;
+    cnt_g = 1 + cnt_a + cnt_c;
+    cnt_c = 1 + cnt_a;
+    cnt_a = 1;
 
     std::string text;
     text.reserve(bwt.size());
     text.push_back('$');
-    text.push_back(bwt[0]);
+    auto cursor = 0;
     while (text.size() < bwt.size()) {
-        if (text.back() == 'A') text.push_back(bwt[pos_a++]);
-        else if (text.back() == 'C') text.push_back(bwt[pos_c++]);
-        else if (text.back() == 'G') text.push_back(bwt[pos_g++]);
-        else if (text.back() == 'T') text.push_back(bwt[pos_t++]);
+        auto c = bwt[cursor];
+        text.push_back(c);
+        if (c == 'A') cursor = cnt_a + a_before[cursor];
+        else if (c == 'C') cursor = cnt_c + c_before[cursor];
+        else if (c == 'G') cursor = cnt_g + g_before[cursor];
+        else if (c == 'T') cursor = cnt_t + t_before[cursor];
     }
     std::reverse(text.begin(), text.end());
     return text;
