2a9f9b40bc122527e2109e349242239dc58c1c47
younghoonJ <to.younghoon.jung@gmail.com>
2023-06-22 23:39:58 +0900

    week3 first commit

---
+++ Data_Structures_and_Algorithms_Specialization/Advanced Algorithms and Complexity/week3/gsm_network.cpp
@@ -0,0 +1,58 @@
+#include <ios>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Edge {
+    int from;
+    int to;
+};
+
+struct RGB {
+    int b, g, r;
+
+    explicit RGB(int i) : b(i * 3), g(b - 1), r(b - 2) {}
+};
+
+struct ConvertGSMNetworkProblemToSat {
+    int numVertices;
+    vector<Edge> edges;
+
+    ConvertGSMNetworkProblemToSat(int n, int m) : numVertices(n), edges(m) {}
+
+    void printEquisatisfiableSatFormula() {
+        // each vertex has one and only one color
+        for (auto i = 1; i < numVertices + 1; ++i) {
+            auto v = RGB(i);
+            cout << v.r << ' ' << v.g << ' ' << v.b << " 0\n"; // r or g or b
+            cout << -v.r << ' ' << -v.g << " 0\n";             // not r or not g;
+            cout << -v.r << ' ' << -v.b << " 0\n";
+            cout << -v.g << ' ' << -v.b << " 0\n";
+        }
+
+        // two verticies at the end of each edge should have different colors
+        for (const auto &e: edges) {
+            auto from = RGB(e.from);
+            auto to = RGB(e.to);
+            cout << -from.r << ' ' << -to.r << " 0\n";
+            cout << -from.g << ' ' << -to.g << " 0\n";
+            cout << -from.b << ' ' << -to.b << " 0\n";
+        }
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    int n, m;
+    cin >> n >> m;
+    ConvertGSMNetworkProblemToSat converter(n, m);
+    for (int i = 0; i < m; ++i) {
+        cin >> converter.edges[i].from >> converter.edges[i].to;
+    }
+
+    converter.printEquisatisfiableSatFormula();
+
+    return 0;
+}
